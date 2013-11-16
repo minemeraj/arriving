0ebb51ab062af19a94306f6242b45d96e59ff669
francescopiazza <francescopiazza@192.168.1.232>
2013-11-16 18:35:14 +0100

    f

--- Driving/src/com/main/CarFrame.java
+++ Driving/src/com/main/CarFrame.java
@@ -42,7 +42,7 @@ public class CarFrame extends JFrame implements KeyListener,ActionListener {
 
 	
     int counter=0;
-	String VERSION="CarDriving 7.0.7";
+	String VERSION="CarDriving 7.0.9";
 	
 	JPanel center=null;
 	private Graphics2D graphics2D;
