4d899e39f8d15ff563c75be6b9cd4149760545dd
francescopiazza <francescopiazza@francesco999.homenet.telecomitalia.it>
2014-10-04 18:36:57 +0200

    f

--- Driving/src/com/editors/forniture/data/Barrel.java
+++ Driving/src/com/editors/forniture/data/Barrel.java
@@ -22,6 +22,9 @@ public class Barrel extends CustomData{
 	
 	public static int texture_x0=10;
 	public static int texture_y0=10;
+	public static int IMG_WIDTH;
+	public static int IMG_HEIGHT;
+	
 
 	public static void saveBarrelTexture(PolygonMesh mesh, File file, Hashtable specificData) {
 		
@@ -32,20 +35,26 @@ public class Barrel extends CustomData{
 		double upper_radius=((Double)specificData.get("UPPER_RADIUS")).doubleValue();
 		double zHeight=((Double)specificData.get("HEIGHT")).doubleValue();
 		
+		double max_radius=Math.max(lower_radius,upper_radius);
+		double len=max_radius*2*pi;
+		
+		texture_side_dy=(int) (zHeight/(N_PARALLELS-1));
+		texture_side_dx=(int) (len/(N_MERIDIANS));
+		
 
 		Color backgroundColor=Color.green;
 
 		
-		int IMG_WIDTH=(int) zHeight;
-		int IMG_HEIGHT=(int) (zHeight+lower_radius*2+upper_radius*2);
+		IMG_WIDTH=(int) len+texture_x0;
+		IMG_HEIGHT=(int) (zHeight+lower_radius*2+upper_radius*2)+texture_y0;
 
 		
 		BufferedImage buf=new BufferedImage(IMG_WIDTH,IMG_HEIGHT,BufferedImage.TYPE_BYTE_INDEXED);
 	
 		try {
 
-				double xc=0;
-				double yc=0;
+				double xc=lower_radius;
+				double yc=upper_radius+zHeight+2*lower_radius;
 
 				double teta=(2*pi)/(N_MERIDIANS);
 
@@ -57,30 +66,33 @@ public class Barrel extends CustomData{
 
 				//draw lines for reference
 
-				bufGraphics.setColor(new Color(0,0,0));
+				bufGraphics.setColor(Color.RED);
 				bufGraphics.setStroke(new BasicStroke(0.1f));
 				
 				for (int j = 0; j <N_MERIDIANS; j++) {
 
-					double x0= texture_x0+xc+(upper_radius*Math.cos(j*teta));
-					double y0= texture_y0+yc+(upper_radius*Math.sin(j*teta));
+					double x0= calX(xc+(upper_radius*Math.cos(j*teta)));
+					double y0= calY(yc+(upper_radius*Math.sin(j*teta)));
 					
-					double x1= texture_x0+xc+(upper_radius*Math.cos((j+1)*teta));
-					double y1= texture_y0+yc+(upper_radius*Math.sin((j+1)*teta));
+					double x1= calX(xc+(upper_radius*Math.cos((j+1)*teta)));
+					double y1= calY(yc+(upper_radius*Math.sin((j+1)*teta)));
 
 					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y1);	 
 
 				}
 
+				
 				//lowerbase
-
+				yc=lower_radius;
+				bufGraphics.setColor(Color.BLUE);
+				
 				for (int j = N_MERIDIANS-1; j >=0; j--) {
 
-					double x0= texture_x0+xc+(lower_radius*Math.cos(j*teta));
-					double y0= texture_y0+yc+(lower_radius*Math.sin(j*teta));
+					double x0= calX(xc+(lower_radius*Math.cos(j*teta)));
+					double y0= calY(yc+(lower_radius*Math.sin(j*teta)));
 					
-					double x1= texture_x0+xc+(lower_radius*Math.cos((j+1)*teta));
-					double y1= texture_y0+yc+(lower_radius*Math.sin((j+1)*teta));
+					double x1= calX(xc+(lower_radius*Math.cos((j+1)*teta)));
+					double y1= calY(yc+(lower_radius*Math.sin((j+1)*teta)));
 
 					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y1);			
 
@@ -88,20 +100,21 @@ public class Barrel extends CustomData{
 
 
 				//lateral surface
-
+				bufGraphics.setColor(new Color(0,0,0));
+				
 				double dx=texture_side_dx;
 				double dy=texture_side_dy;
 
-				for(int i=0;i<N_PARALLELS;i++){
+				for(int j=0;j<N_PARALLELS-1;j++){
 
 					//texture is open and periodical:
 
-					for (int j = 0; j <N_MERIDIANS; j++) {
+					for (int i = 0; i <N_MERIDIANS; i++) { 
 
-						double x0=texture_x0+dx*i;
-						double x1=texture_x0+dx*(i+1);
-						double y0=texture_y0+dy*j;
-						double y1=texture_y0+dy*(j+1);
+						double x0=calX(dx*i);
+						double x1=calX(dx*(i+1));
+						double y0=calY(2*lower_radius+dy*j);
+						double y1=calY(2*lower_radius+dy*(j+1));
 						
 						bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y0);
 						bufGraphics.drawLine((int)x1,(int)y0,(int)x1,(int)y1);
@@ -191,5 +204,14 @@ public class Barrel extends CustomData{
 		
 		return texture_points;
 	}
+	
+	public static double calX(double x){
+		
+		return texture_x0+x;
+	}
 
+	public static double calY(double y){
+		
+		return IMG_HEIGHT-(texture_y0+y);
+	}
 }
