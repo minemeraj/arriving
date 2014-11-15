e27b157603d65343749d975cee97f944afb575b4
francescopiazza <francescopiazza@192.168.1.232>
2014-11-15 10:08:50 +0100

    f

---
+++ Driving/src/com/editors/forniture/data/Globe1.java
@@ -0,0 +1,394 @@
+package com.editors.forniture.data;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+
+import com.BPoint;
+import com.CustomData;
+import com.LineData;
+import com.Point3D;
+import com.PolygonMesh;
+import com.main.Renderer3D;
+
+public class Globe1 extends CustomData { 
+	
+	double radius=0;
+	private int N_MERIDIANS;
+	private int N_PARALLELS;
+	
+	Point3D[][] lateralFaces=null; 
+	Point3D[]northPoles=null;
+	Point3D[]southPoles=null;
+	
+	public static int texture_side_dx=10;
+	public static int texture_side_dy=10;
+	
+	public static int texture_x0=10;
+	public static int texture_y0=10;
+	public static int IMG_WIDTH;
+	public static int IMG_HEIGHT;
+	
+	public double len;
+	public double zHeight;
+	
+	public static boolean isTextureDrawing=false;
+
+	public Globe1(double radius,int N_MERIDIANS,int N_PARALLELS) {
+		
+		this.radius=radius;
+		this.N_MERIDIANS=N_MERIDIANS;
+		this.N_PARALLELS=N_PARALLELS;
+		
+		zHeight=2*radius;
+		len=2*pi*radius;
+		
+		lateralFaces=new Point3D[N_MERIDIANS+1][N_PARALLELS];
+		
+		texture_side_dy=(int) (zHeight/(N_PARALLELS-1));
+		texture_side_dx=(int) (len/(N_MERIDIANS));
+		
+		double dx=texture_side_dx;
+		double dy=texture_side_dy;
+		
+		northPoles=new Point3D[N_MERIDIANS+1];
+		
+		for (int i = 0; i <=N_MERIDIANS; i++) {
+
+			double x=dx*(i+0.5);
+			double y=2*radius+zHeight;
+
+			northPoles[i]=new Point3D(x,y,0);
+
+		}
+
+		for(int j=0;j<N_PARALLELS;j++){
+
+			//texture is open and periodical:
+
+			for (int i = 0; i <=N_MERIDIANS; i++) {
+
+				double x=dx*i;
+				double y=radius+zHeight-dy*j;
+
+				lateralFaces[i][j]=new Point3D(x,y,0);
+
+			}
+			
+		}	
+		
+	
+		southPoles=new Point3D[N_MERIDIANS+1];
+		
+		for (int i = 0; i <=N_MERIDIANS; i++) {
+
+			double x=dx*(i+0.5);
+			double y=0;
+
+			southPoles[i]=new Point3D(x,y,0);
+
+		}
+		
+		initMesh();
+	}
+	
+	public void initMesh( ) {
+
+
+		Hashtable values=new Hashtable();
+
+		points=new Vector();
+		points.setSize(200);
+		polyData=new Vector();
+
+		n=0;
+
+		double fi=(2*pi)/(N_MERIDIANS);
+		double teta=pi/(N_PARALLELS+1);
+		
+		BPoint northPole=addBPoint(0,0,radius);
+
+		BPoint[][] aPoints=new BPoint[N_MERIDIANS][N_PARALLELS];
+
+		for (int i = 0; i < N_MERIDIANS; i++) {
+
+			for (int j = 0; j <N_PARALLELS; j++) {
+
+				double x= radius*Math.cos(i*fi)*Math.sin(teta+j*teta);
+				double y= radius*Math.sin(i*fi)*Math.sin(teta+j*teta);
+				double z= radius*Math.cos(teta+j*teta);
+
+				aPoints[i][j]=
+						addBPoint(x,y,z);
+
+				points.setElementAt(aPoints[i][j],aPoints[i][j].getIndex());
+
+			}
+		}
+		
+		BPoint southPole=addBPoint(0,0,-radius);
+		
+		texture_points=buildTexturePoints();
+	
+
+		int count=N_MERIDIANS;
+
+		for (int i = 0; i <N_MERIDIANS; i++) { 
+			
+			LineData ld=new LineData();
+
+			int texIndex=count+f(i,0,N_MERIDIANS+1,N_PARALLELS);
+			//System.out.print(texIndex+"\t");
+			ld.addIndex(aPoints[i][0].getIndex(),texIndex,0,0);
+
+			texIndex=count+f(i+1,0,N_MERIDIANS+1,N_PARALLELS);
+			//System.out.print(texIndex+"\t");
+			ld.addIndex(aPoints[(i+1)%N_MERIDIANS][0].getIndex(),texIndex,0,0);
+
+			
+			ld.addIndex(northPole.getIndex(),i,0,0);
+
+			ld.setData(""+Renderer3D.getFace(ld,points));
+
+
+
+			polyData.add(ld);
+		}
+
+		for (int i = 0; i <N_MERIDIANS; i++) { 
+
+			for(int j=0;j<N_PARALLELS-1;j++){ 
+
+				LineData ld=new LineData();
+
+				int texIndex=count+f(i,j,N_MERIDIANS+1,N_PARALLELS);
+				//System.out.print(texIndex+"\t");
+				ld.addIndex(aPoints[i][j].getIndex(),texIndex,0,0);
+				
+				texIndex=count+f(i,j+1,N_MERIDIANS+1,N_PARALLELS);
+				//System.out.print(texIndex+"\t");
+				ld.addIndex(aPoints[i][j+1].getIndex(),texIndex,0,0);
+				
+				texIndex=count+f(i+1,j+1,N_MERIDIANS+1,N_PARALLELS);
+				//System.out.print(texIndex+"\t");
+				ld.addIndex(aPoints[(i+1)%N_MERIDIANS][j+1].getIndex(),texIndex,0,0);
+
+				texIndex=count+f(i+1,j,N_MERIDIANS+1,N_PARALLELS);
+				//System.out.print(texIndex+"\t");
+				ld.addIndex(aPoints[(i+1)%N_MERIDIANS][j].getIndex(),texIndex,0,0);
+
+
+				ld.setData(""+Renderer3D.getFace(ld,points));
+
+
+
+				polyData.add(ld);
+
+			}
+
+
+
+		}
+		
+		int spIndex=N_MERIDIANS+N_PARALLELS*(N_MERIDIANS+1);
+		
+		for (int i = 0; i <N_MERIDIANS; i++) { 
+			
+			LineData ld=new LineData();			
+
+			int texIndex=count+f(i,(N_PARALLELS-1),N_MERIDIANS+1,N_PARALLELS);
+			//System.out.print(texIndex+"\t");
+			ld.addIndex(aPoints[i][(N_PARALLELS-1)].getIndex(),texIndex,0,0);
+			
+			ld.addIndex(southPole.getIndex(),spIndex+i,0,0);
+
+			texIndex=count+f(i+1,(N_PARALLELS-1),N_MERIDIANS+1,N_PARALLELS);
+			//System.out.print(texIndex+"\t");
+			ld.addIndex(aPoints[(i+1)%N_MERIDIANS][(N_PARALLELS-1)].getIndex(),texIndex,0,0);
+		
+
+			ld.setData(""+Renderer3D.getFace(ld,points));
+
+
+
+			polyData.add(ld);
+		}
+	}	
+	
+	public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
+	
+		isTextureDrawing=true;
+		
+	
+		
+		Color backgroundColor=Color.green;
+
+		
+		IMG_WIDTH=(int) len+2*texture_x0;
+		IMG_HEIGHT=(int) (zHeight+radius*2)+2*texture_y0;
+
+		
+		BufferedImage buf=new BufferedImage(IMG_WIDTH,IMG_HEIGHT,BufferedImage.TYPE_BYTE_INDEXED);
+	
+		try {
+
+
+				Graphics2D bufGraphics=(Graphics2D)buf.getGraphics();
+ 
+				bufGraphics.setColor(backgroundColor);
+				bufGraphics.fillRect(0,0,IMG_WIDTH,IMG_HEIGHT);
+				
+				bufGraphics.setColor(Color.RED);
+				bufGraphics.setStroke(new BasicStroke(0.1f));
+		
+	
+				for (int i = 0; i <N_MERIDIANS; i++) { 
+						
+						double x0=calX(lateralFaces[i][N_PARALLELS-1].x);
+						double x1=calX(lateralFaces[i+1][N_PARALLELS-1].x);
+						double y0=calY(lateralFaces[i][N_PARALLELS-1].y);
+						
+						double xp=calX(northPoles[i].x);
+						double yp=calY(northPoles[i].y);
+						
+						bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y0);
+						bufGraphics.drawLine((int)x1,(int)y0,(int)xp,(int)yp);
+						bufGraphics.drawLine((int)xp,(int)yp,(int)x0,(int)y0);
+				}
+		
+				//lateral surface
+				bufGraphics.setColor(new Color(0,0,0));
+
+				for(int j=0;j<N_PARALLELS-1;j++){
+
+					//texture is open and periodical:
+
+					for (int i = 0; i <N_MERIDIANS; i++) { 
+
+						double x0=calX(lateralFaces[i][j].x);
+						double x1=calX(lateralFaces[i+1][j].x);
+						double y0=calY(lateralFaces[i][j].y);
+						double y1=calY(lateralFaces[i][j+1].y);
+						
+						bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y0);
+						bufGraphics.drawLine((int)x1,(int)y0,(int)x1,(int)y1);
+						bufGraphics.drawLine((int)x1,(int)y1,(int)x0,(int)y1);
+						bufGraphics.drawLine((int)x0,(int)y1,(int)x0,(int)y0);
+					}
+
+				}	
+				
+				bufGraphics.setColor(Color.BLUE);
+				bufGraphics.setStroke(new BasicStroke(0.1f));
+				
+				for (int i = 0; i <N_MERIDIANS; i++) { 
+					
+					double x0=calX(lateralFaces[i][0].x);
+					double x1=calX(lateralFaces[i+1][0].x);
+					double y0=calY(lateralFaces[i][0].y);
+					
+					double xp=calX(southPoles[i].x);
+					double yp=calY(southPoles[i].y);
+					
+					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y0);
+					bufGraphics.drawLine((int)x1,(int)y0,(int)xp,(int)yp);
+					bufGraphics.drawLine((int)xp,(int)yp,(int)x0,(int)y0);
+			    }
+				
+			
+				ImageIO.write(buf,"gif",file);
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+public Vector buildTexturePoints() {
+	
+	isTextureDrawing=false;
+	
+	Vector texture_points=new Vector();
+	
+	double teta=(2*pi)/(N_MERIDIANS);
+
+
+	int size=2*N_MERIDIANS+	(N_MERIDIANS+1)*(N_PARALLELS);
+
+	
+	texture_points.setSize(size);
+	
+	texture_side_dy=(int) (zHeight/(N_PARALLELS-1));
+	texture_side_dx=(int) (len/(N_MERIDIANS));
+	
+	IMG_WIDTH=(int) len+2*texture_x0;
+	IMG_HEIGHT=(int) (zHeight+radius*2)+2*texture_y0;
+
+	int count=0;
+
+	
+	for (int i = 0; i <N_MERIDIANS; i++) { 
+		
+		double xp=calX(northPoles[i].x);
+		double yp=calY(northPoles[i].y);
+		
+		Point3D p=new Point3D(xp,yp,0);	
+		texture_points.setElementAt(p,count++);
+}
+
+	//lateral surface
+
+
+	for(int j=0;j<N_PARALLELS;j++){
+
+		//texture is open and periodical:
+
+		for (int i = 0; i <=N_MERIDIANS; i++) {
+
+			double x=calX(lateralFaces[i][j].x);
+			double y=calY(lateralFaces[i][j].y);
+
+			Point3D p=new Point3D(x,y,0);
+
+			int texIndex=count+f(i,j,N_MERIDIANS+1,N_PARALLELS);
+			//System.out.print(texIndex+"\t");
+			texture_points.setElementAt(p,texIndex);
+		}
+		
+	}	
+	
+	count=N_MERIDIANS+N_PARALLELS*(N_MERIDIANS+1);
+	
+	for (int i = 0; i <N_MERIDIANS; i++) { 
+		
+		double xp=calX(southPoles[i].x);
+		double yp=calY(southPoles[i].y);
+		
+		Point3D p=new Point3D(xp,yp,0);	
+		texture_points.setElementAt(p,count++);
+
+    }
+	
+	return texture_points;
+}
+
+		public static double calX(double x){
+			
+			return texture_x0+x;
+		}
+		
+		public static double calY(double y){
+			if(isTextureDrawing)
+				return IMG_HEIGHT-(texture_y0+y);
+			else
+				return texture_y0+y;
+		}
+
+}
