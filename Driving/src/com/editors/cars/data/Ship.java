9e51bd8af0f8ee2dccd2fc408ce9a2913492a0be
francescopiazza <francescopiazza@192.168.1.232>
2014-11-16 11:06:49 +0100

    f

--- Driving/src/com/editors/cars/data/Ship.java
+++ Driving/src/com/editors/cars/data/Ship.java
@@ -2,7 +2,11 @@ package com.editors.cars.data;
 
 import java.util.Vector;
 
+import com.BPoint;
 import com.CustomData;
+import com.LineData;
+import com.Segments;
+import com.main.Renderer3D;
 
 public class Ship extends CustomData {
 	
@@ -21,6 +25,140 @@ public class Ship extends CustomData {
 
 		n=0;
 		
+		int ny=11;
+		int nz=4;
+		
+		BPoint[][][] rightBroadSide=new BPoint[1][ny][nz];	 
+		
+		Segments s0=new Segments(0,100,0,400,0,100);
+		
+		rightBroadSide[0][0][0]=addBPoint(0,0,0,s0);
+		rightBroadSide[0][0][1]=addBPoint(0.0,0,0.25,s0);
+		rightBroadSide[0][0][2]=addBPoint(0.0,0,0.5,s0);
+		rightBroadSide[0][0][3]=addBPoint(0.0,0,0.75,s0);
+		
+		rightBroadSide[0][1][0]=addBPoint(0,0.1,0,s0);
+		rightBroadSide[0][1][1]=addBPoint(0.525,0.1,0.25,s0);
+		rightBroadSide[0][1][2]=addBPoint(0.65,0.1,0.5,s0);
+		rightBroadSide[0][1][3]=addBPoint(0.75,0.1,0.75,s0);
+		
+		rightBroadSide[0][2][0]=addBPoint(0,0.2,0,s0);
+		rightBroadSide[0][2][1]=addBPoint(0.525,0.2,0.25,s0);
+		rightBroadSide[0][2][2]=addBPoint(0.65,0.2,0.5,s0);
+		rightBroadSide[0][2][3]=addBPoint(0.75,0.2,0.75,s0);
+		
+		rightBroadSide[0][3][0]=addBPoint(0,0.3,0,s0);
+		rightBroadSide[0][3][1]=addBPoint(0.525,0.3,0.25,s0);
+		rightBroadSide[0][3][2]=addBPoint(0.65,0.3,0.5,s0);
+		rightBroadSide[0][3][3]=addBPoint(0.75,0.3,0.75,s0);
+		
+		rightBroadSide[0][4][0]=addBPoint(0,0.4,0,s0);
+		rightBroadSide[0][4][1]=addBPoint(0.525,0.4,0.25,s0);
+		rightBroadSide[0][4][2]=addBPoint(0.65,0.4,0.5,s0);
+		rightBroadSide[0][4][3]=addBPoint(0.75,0.4,0.75,s0);
+		
+		rightBroadSide[0][5][0]=addBPoint(0,0.5,0,s0);
+		rightBroadSide[0][5][1]=addBPoint(0.525,0.5,0.25,s0);
+		rightBroadSide[0][5][2]=addBPoint(0.65,0.5,0.5,s0);
+		rightBroadSide[0][5][3]=addBPoint(0.75,0.5,0.75,s0);
+		
+		rightBroadSide[0][6][0]=addBPoint(0,0.6,0,s0);
+		rightBroadSide[0][6][1]=addBPoint(0.525,0.6,0.25,s0);
+		rightBroadSide[0][6][2]=addBPoint(0.65,0.6,0.5,s0);
+		rightBroadSide[0][6][3]=addBPoint(0.75,0.6,0.75,s0);
+		
+		rightBroadSide[0][7][0]=addBPoint(0,0.7,0,s0);
+		rightBroadSide[0][7][1]=addBPoint(0.525,0.7,0.25,s0);
+		rightBroadSide[0][7][2]=addBPoint(0.65,0.7,0.5,s0);
+		rightBroadSide[0][7][3]=addBPoint(0.75,0.7,0.75,s0);
+		
+		rightBroadSide[0][8][0]=addBPoint(0,0.8,0,s0);
+		rightBroadSide[0][8][1]=addBPoint(0.525,0.8,0.25,s0);
+		rightBroadSide[0][8][2]=addBPoint(0.65,0.8,0.5,s0);
+		rightBroadSide[0][8][3]=addBPoint(0.75,0.8,0.75,s0);
+		
+		rightBroadSide[0][9][0]=addBPoint(0,0.9,0,s0);
+		rightBroadSide[0][9][1]=addBPoint(0.525,0.9,0.25,s0);
+		rightBroadSide[0][9][2]=addBPoint(0.65,0.9,0.5,s0);
+		rightBroadSide[0][9][3]=addBPoint(0.75,0.9,0.75,s0);
+		
+		rightBroadSide[0][10][0]=addBPoint(0,1.0,0,s0);
+		rightBroadSide[0][10][1]=addBPoint(0.0,1.0,0.25,s0);
+		rightBroadSide[0][10][2]=addBPoint(0.0,1.0,0.5,s0);
+		rightBroadSide[0][10][3]=addBPoint(0.0,1.0,0.75,s0);
+		
+		for (int j = 0; j < ny-1; j++) { 
+			
+			for (int k = 0; k < nz-1; k++) {
+				LineData leftLD=addLine(rightBroadSide[0][j][k],rightBroadSide[0][j+1][k],rightBroadSide[0][j+1][k+1],rightBroadSide[0][j][k+1],Renderer3D.CAR_RIGHT);
+			}
+			
+		}
+		
+		BPoint[][][] leftBroadSide=new BPoint[1][ny][nz];	 
+		
+		leftBroadSide[0][0][0]=addBPoint(0,0,0,s0);
+		leftBroadSide[0][0][1]=addBPoint(0.0,0,0.25,s0);
+		leftBroadSide[0][0][2]=addBPoint(0.0,0,0.5,s0);
+		leftBroadSide[0][0][3]=addBPoint(0.0,0,0.75,s0);
+		
+		leftBroadSide[0][1][0]=addBPoint(0,0.1,0,s0);
+		leftBroadSide[0][1][1]=addBPoint(-0.525,0.1,0.25,s0);
+		leftBroadSide[0][1][2]=addBPoint(-0.65,0.1,0.5,s0);
+		leftBroadSide[0][1][3]=addBPoint(-0.75,0.1,0.75,s0);
+		
+		leftBroadSide[0][2][0]=addBPoint(0,0.2,0,s0);
+		leftBroadSide[0][2][1]=addBPoint(-0.525,0.2,0.25,s0);
+		leftBroadSide[0][2][2]=addBPoint(-0.65,0.2,0.5,s0);
+		leftBroadSide[0][2][3]=addBPoint(-0.75,0.2,0.75,s0);
+		
+		leftBroadSide[0][3][0]=addBPoint(0,0.3,0,s0);
+		leftBroadSide[0][3][1]=addBPoint(-0.525,0.3,0.25,s0);
+		leftBroadSide[0][3][2]=addBPoint(-0.65,0.3,0.5,s0);
+		leftBroadSide[0][3][3]=addBPoint(-0.75,0.3,0.75,s0);
+		
+		leftBroadSide[0][4][0]=addBPoint(0,0.4,0,s0);
+		leftBroadSide[0][4][1]=addBPoint(-0.525,0.4,0.25,s0);
+		leftBroadSide[0][4][2]=addBPoint(-0.65,0.4,0.5,s0);
+		leftBroadSide[0][4][3]=addBPoint(-0.75,0.4,0.75,s0);
+		
+		leftBroadSide[0][5][0]=addBPoint(0,0.5,0,s0);
+		leftBroadSide[0][5][1]=addBPoint(-0.525,0.5,0.25,s0);
+		leftBroadSide[0][5][2]=addBPoint(-0.65,0.5,0.5,s0);
+		leftBroadSide[0][5][3]=addBPoint(-0.75,0.5,0.75,s0);
+		
+		leftBroadSide[0][6][0]=addBPoint(0,0.6,0,s0);
+		leftBroadSide[0][6][1]=addBPoint(-0.525,0.6,0.25,s0);
+		leftBroadSide[0][6][2]=addBPoint(-0.65,0.6,0.5,s0);
+		leftBroadSide[0][6][3]=addBPoint(-0.75,0.6,0.75,s0);
+		
+		leftBroadSide[0][7][0]=addBPoint(0,0.7,0,s0);
+		leftBroadSide[0][7][1]=addBPoint(-0.525,0.7,0.25,s0);
+		leftBroadSide[0][7][2]=addBPoint(-0.65,0.7,0.5,s0);
+		leftBroadSide[0][7][3]=addBPoint(-0.75,0.7,0.75,s0);
+		
+		leftBroadSide[0][8][0]=addBPoint(0,0.8,0,s0);
+		leftBroadSide[0][8][1]=addBPoint(-0.525,0.8,0.25,s0);
+		leftBroadSide[0][8][2]=addBPoint(-0.65,0.8,0.5,s0);
+		leftBroadSide[0][8][3]=addBPoint(-0.75,0.8,0.75,s0);
+		
+		leftBroadSide[0][9][0]=addBPoint(0,0.9,0,s0);
+		leftBroadSide[0][9][1]=addBPoint(-0.525,0.9,0.25,s0);
+		leftBroadSide[0][9][2]=addBPoint(-0.65,0.9,0.5,s0);
+		leftBroadSide[0][9][3]=addBPoint(-0.75,0.9,0.75,s0);
+		
+		leftBroadSide[0][10][0]=addBPoint(0,1.0,0,s0);
+		leftBroadSide[0][10][1]=addBPoint(-0.0,1.0,0.25,s0);
+		leftBroadSide[0][10][2]=addBPoint(-0.0,1.0,0.5,s0);
+		leftBroadSide[0][10][3]=addBPoint(-0.0,1.0,0.75,s0);
+		
+		for (int j = 0; j < ny-1; j++) { 
+			
+			for (int k = 0; k < nz-1; k++) {
+				LineData leftLD=addLine(leftBroadSide[0][j][k],leftBroadSide[0][j][k+1],leftBroadSide[0][j+1][k+1],leftBroadSide[0][j+1][k],Renderer3D.CAR_LEFT);
+			}
+			
+		}
 	}
 
 }
