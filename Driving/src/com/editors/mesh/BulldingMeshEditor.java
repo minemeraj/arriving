87bab45f14815b94e35ca0aeeeef49b82627582f
francescopiazza <francescopiazza@francesco999.homenet.telecomitalia.it>
2015-06-30 20:55:57 +0200

    f

--- Driving/src/com/editors/mesh/BulldingMeshEditor.java
+++ Driving/src/com/editors/mesh/BulldingMeshEditor.java
@@ -1,197 +1,216 @@
 package com.editors.mesh;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.PrintWriter;
 
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JRadioButton;
 
 import com.editors.DoubleTextField;
-import com.editors.models.CubeModel;
+import com.editors.ValuePair;
 import com.editors.models.Gambrel0Model;
 import com.editors.models.House0Model;
 import com.editors.models.House1Model;
 import com.editors.models.Mansard0Model;
-import com.editors.models.TableModel;
 
-public class BulldingMeshEditor extends MeshModelEditor{
-	
+public class BulldingMeshEditor extends MeshModelEditor implements KeyListener{
+
 	String title="Building model";
 	private DoubleTextField dz_text;
 	private DoubleTextField dy_text;
 	private DoubleTextField dx_text;
-	private JRadioButton jcbHouse0;
 	private DoubleTextField roof_height;
-	private JRadioButton jcbHouse1;
 	private DoubleTextField dx1_text;
 	private DoubleTextField dy1_text;
-	private JRadioButton jcbGambrel0;
-	private JRadioButton jcbMansard0;
+	private JComboBox chooseBuilding;
+	
+	public static int HOUSE0=0;
+	public static int HOUSE1=1;
+	public static int GAMBREL0=2;
+	public static int MANSARD0=3;
 	
 	public static void main(String[] args) {
-		
+
 		BulldingMeshEditor fm=new BulldingMeshEditor(470,290);
 	}
-	
-	
+
+
 	public BulldingMeshEditor(int W, int H) {
 		super(W, H);
 		setTitle(title);
 	}
 
+
+	public void buildCenter() {
+
+		double dx=100;
+		double dy=200;
+		double dz=50;
+		double dr=50;
+		double dx1=50;
+		double dy1=150;
+
+		int r=10;
+
+		int c0=220;
+		int c1=310;
+
+		JLabel lx=new JLabel("dx:");
+		lx.setBounds(5,r,80,20);
+		center.add(lx);
+		dx_text=new DoubleTextField(8);
+		dx_text.setBounds(90,r,120,20);
+		dx_text.setText(dx);
+		center.add(dx_text);		
+
+		lx=new JLabel("dx1:");
+		lx.setBounds(c0,r,80,20);
+		center.add(lx);
+		dx1_text=new DoubleTextField(8);
+		dx1_text.setBounds(c1,r,120,20);
+		dx1_text.setText(dx1);
+		center.add(dx1_text);
+
+		r+=30;
+
+		JLabel ly=new JLabel("dy:");
+		ly.setBounds(5,r,80,20);
+		center.add(ly);
+		dy_text=new DoubleTextField(8);
+		dy_text.setBounds(90,r,120,20);
+		dy_text.setText(dy);
+		center.add(dy_text);
+
+		ly=new JLabel("dy1:");
+		ly.setBounds(c0,r,80,20);
+		center.add(ly);
+		dy1_text=new DoubleTextField(8);
+		dy1_text.setBounds(c1,r,120,20);
+		dy1_text.setText(dy1);
+		center.add(dy1_text);
+
+
+		r+=30;
+
+		JLabel lz=new JLabel("dz:");
+		lz.setBounds(5,r,80,20);
+		center.add(lz);
+		dz_text=new DoubleTextField(8);
+		dz_text.setBounds(90,r,120,20);
+		dz_text.setText(dz);
+		center.add(dz_text);
+
+		r+=30;
+
+
+		JLabel lr=new JLabel("Roof h:");
+		lr.setBounds(5,r,80,20);
+		center.add(lr);
+		roof_height=new DoubleTextField(8);
+		roof_height.setBounds(90,r,120,20);
+		roof_height.setText(dr);
+		center.add(roof_height);
+
 	
-	 public void buildCenter() {
+		r+=30;
 
-		 double dx=100;
-		 double dy=200;
-		 double dz=50;
-		 double dr=50;
-		 double dx1=50;
-		 double dy1=150;
-		 
-		 int r=10;
-		 
-		 int c0=220;
-		 int c1=310;
+		JLabel jlb=new JLabel("Buiilding type:");
+		jlb.setBounds(5, r, 100, 20);
+		center.add(jlb);
 
-		 JLabel lx=new JLabel("dx:");
-		 lx.setBounds(5,r,80,20);
-		 center.add(lx);
-		 dx_text=new DoubleTextField(8);
-		 dx_text.setBounds(90,r,120,20);
-		 dx_text.setText(dx);
-		 center.add(dx_text);		
-		 
-		 lx=new JLabel("dx1:");
-		 lx.setBounds(c0,r,80,20);
-		 center.add(lx);
-		 dx1_text=new DoubleTextField(8);
-		 dx1_text.setBounds(c1,r,120,20);
-		 dx1_text.setText(dx1);
-		 center.add(dx1_text);
+		chooseBuilding=new JComboBox();
+		chooseBuilding.setBounds(115, r, 100, 20);
+		chooseBuilding.addKeyListener(this);
+		chooseBuilding.addItem(new ValuePair("-1",""));
+		chooseBuilding.addItem(new ValuePair(""+HOUSE0,House0Model.NAME));		
+		chooseBuilding.addItem(new ValuePair(""+HOUSE1,House1Model.NAME));	
+		chooseBuilding.addItem(new ValuePair(""+GAMBREL0,Gambrel0Model.NAME));	
+		chooseBuilding.addItem(new ValuePair(""+MANSARD0,Mansard0Model.NAME));	
+		
+	
 
-		 r+=30;
+		chooseBuilding.setSelectedIndex(0);
+		center.add(chooseBuilding);
 
-		 JLabel ly=new JLabel("dy:");
-		 ly.setBounds(5,r,80,20);
-		 center.add(ly);
-		 dy_text=new DoubleTextField(8);
-		 dy_text.setBounds(90,r,120,20);
-		 dy_text.setText(dy);
-		 center.add(dy_text);
-		 
-		 ly=new JLabel("dy1:");
-		 ly.setBounds(c0,r,80,20);
-		 center.add(ly);
-		 dy1_text=new DoubleTextField(8);
-		 dy1_text.setBounds(c1,r,120,20);
-		 dy1_text.setText(dy1);
-		 center.add(dy1_text);
+		r+=30;
 
+		meshButton=new JButton("Mesh");
+		meshButton.setBounds(10,r,80,20);
+		meshButton.addActionListener(this);
+		center.add(meshButton);
 
-		 r+=30;
+		r+=30;
 
-		 JLabel lz=new JLabel("dz:");
-		 lz.setBounds(5,r,80,20);
-		 center.add(lz);
-		 dz_text=new DoubleTextField(8);
-		 dz_text.setBounds(90,r,120,20);
-		 dz_text.setText(dz);
-		 center.add(dz_text);
-		 
-		 r+=30;
-		 
+		textureButton=new JButton("Texture");
+		textureButton.setBounds(10,r,90,20);
+		textureButton.addActionListener(this);
+		center.add(textureButton);
 
-		 JLabel lr=new JLabel("Roof h:");
-		 lr.setBounds(5,r,80,20);
-		 center.add(lr);
-		 roof_height=new DoubleTextField(8);
-		 roof_height.setBounds(90,r,120,20);
-		 roof_height.setText(dr);
-		 center.add(roof_height);
-		 
-		 r+=30;	 
-		 
-		 
-		 jcbHouse0=new JRadioButton(House0Model.NAME);
-		 jcbHouse0.setBounds(5,r,80,20);
-		 jcbHouse0.setSelected(true);
-		 center.add(jcbHouse0);
-		 
-		 jcbGambrel0=new JRadioButton(Gambrel0Model.NAME);
-		 jcbGambrel0.setBounds(90,r,80,20);
-		 jcbGambrel0.setSelected(true);
-		 center.add(jcbGambrel0);	
-
-		 r+=30;
-		 
-		 jcbHouse1=new JRadioButton(House1Model.NAME);
-		 jcbHouse1.setBounds(5,r,80,20);
-		 jcbHouse1.setSelected(true);
-		 center.add(jcbHouse1);
-		 
-		 jcbMansard0=new JRadioButton(Mansard0Model.NAME);
-		 jcbMansard0.setBounds(90,r,90,20);
-		 center.add(jcbMansard0);	
-		 
-		 ButtonGroup bg=new ButtonGroup();
-		 bg.add(jcbHouse0);
-		 bg.add(jcbHouse1);		 
-		 bg.add(jcbGambrel0);
-		 bg.add(jcbMansard0);
-		 
-		 r+=30;
-
-		 meshButton=new JButton("Mesh");
-		 meshButton.setBounds(10,r,80,20);
-		 meshButton.addActionListener(this);
-		 center.add(meshButton);
-
-		 r+=30;
-
-		 textureButton=new JButton("Texture");
-		 textureButton.setBounds(10,r,90,20);
-		 textureButton.addActionListener(this);
-		 center.add(textureButton);
-
-	 }
+	}
 
 	public void initMesh() {
+
+
+		double dx = dx_text.getvalue();
+		double dy = dy_text.getvalue();
+		double dz = dz_text.getvalue();
+		double rh = roof_height.getvalue();
+
+		double dx1 = dx1_text.getvalue();
+		double dy1 = dy1_text.getvalue();
 		
-	    	
-	    double dx = dx_text.getvalue();
-	    double dy = dy_text.getvalue();
-	    double dz = dz_text.getvalue();
-	    double rh = roof_height.getvalue();
+	    ValuePair vp= (ValuePair)chooseBuilding.getSelectedItem();
 	    
-	    double dx1 = dx1_text.getvalue();
-	    double dy1 = dy1_text.getvalue();
-	    
-	    if(jcbHouse0.isSelected())
-	    	meshModel=new House0Model(dx,dy,dz,rh,dy1);
-	    else if(jcbHouse1.isSelected())
-	    	meshModel=new House1Model(dx,dy,dz,rh,dx1,dy1);
-	    else if(jcbMansard0.isSelected())
-	    	meshModel=new Mansard0Model(dx,dy,dz,rh,dx1,dy1);
-	    else if(jcbGambrel0.isSelected())
-	    	meshModel=new Gambrel0Model(dx,dy,dz);
-	    	
-	    
-	    meshModel.initMesh();
+	    int val=Integer.parseInt(vp.getId());
+	    if(val<0)
+	    	val=HOUSE0;
+
+		if(HOUSE0==val)
+			meshModel=new House0Model(dx,dy,dz,rh,dy1);
+		else if(HOUSE1==val)
+			meshModel=new House1Model(dx,dy,dz,rh,dx1,dy1);
+		else if(MANSARD0==val)
+			meshModel=new Mansard0Model(dx,dy,dz,rh,dx1,dy1);
+		else if(GAMBREL0==val)
+			meshModel=new Gambrel0Model(dx,dy,dz);
+
+
+		meshModel.initMesh();
 	}
-	
+
 	public void printTexture(File file){
 
 		meshModel.printTexture(file);
 
 	}
-	
+
 	public void printMeshData(PrintWriter pw) { 
 
 		meshModel.printMeshData(pw);
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
