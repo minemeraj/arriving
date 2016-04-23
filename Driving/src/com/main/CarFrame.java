23bebc4e5679a230a6b7e8bc1f9fe9bea1e6dea4
francesco <francescopiazza999@gmail.com>
2016-04-23 17:32:21 +0200

    f

--- Driving/src/com/main/CarFrame.java
+++ Driving/src/com/main/CarFrame.java
@@ -31,7 +31,7 @@ import com.sound.GameSound;
 public class CarFrame extends Road implements KeyListener {
 
 	
-	private String VERSION="CarDriving 9.0.2";
+	private String VERSION="CarDriving 9.0.3";
 	
 	private JPanel center=null;
 	private Graphics2D graphics2D;
