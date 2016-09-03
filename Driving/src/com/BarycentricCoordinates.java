0d2686be413f98b93d1b3fca48a0c847187808c7
francesco <francescopiazza999@gmail.com>
2016-09-03 17:07:02 +0200

    F

--- Driving/src/com/BarycentricCoordinates.java
+++ Driving/src/com/BarycentricCoordinates.java
@@ -118,7 +118,7 @@ public class BarycentricCoordinates {
 			double x2,double y2
 			){
 
-		return Math.abs((x1*y2-x2*y1)-(x0*y2-x2*y0)+(x0*y1-y0*x1));
+		return 0.5*Math.abs((x1*y2-x2*y1)-(x0*y2-x2*y0)+(x0*y1-y0*x1));
 
 	}
 
