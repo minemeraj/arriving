959418a366ed4b32f25156a693b7eaffc7789fee
francesco <francescopiazza999@gmail.com>
2016-09-17 16:39:31 +0200

    Object3D_2 revision

--- Driving/src/com/editors/forniture/data/Wardrobe.java
+++ Driving/src/com/editors/forniture/data/Wardrobe.java
@@ -16,95 +16,95 @@ import com.PolygonMesh;
 import com.main.Renderer3D;
 
 class Wardrobe extends Forniture{
-	
+
 	private int N_FACES=4;
 	private int N_PARALLELS=2;
-	
+
 	private Point3D[] upperBase=null;
 	private Point3D[] lowerBase=null;
-	private Point3D[][] lateralFaces=null; 
-	
+	private Point3D[][] lateralFaces=null;
+
 
 	Wardrobe(double x_side, double y_side, double z_side) {
-		
+
 		this.x_side=x_side;
 		this.y_side=y_side;
 		this.z_side=z_side;
-		
-		len=2*(x_side+y_side);
-		
-		upperBase=new Point3D[N_FACES];
-		
-		upperBase[0]=new Point3D(0,y_side+z_side,0);			
-		upperBase[1]=new Point3D(x_side,y_side+z_side,0);	
-		upperBase[2]=new Point3D(x_side,y_side+z_side+y_side,0);	
-		upperBase[3]=new Point3D(0,y_side+z_side+y_side,0);	
-		
-		lowerBase=new Point3D[N_FACES];
-		
-		lowerBase[0]=new Point3D(0,0,0);			
-		lowerBase[1]=new Point3D(0,y_side,0);	
-		lowerBase[2]=new Point3D(x_side,y_side,0);	
-		lowerBase[3]=new Point3D(x_side,0,0);	
-		
 
-		
+		len=2*(x_side+y_side);
+
+		upperBase=new Point3D[N_FACES];
+
+		upperBase[0]=new Point3D(0,y_side+z_side,0);
+		upperBase[1]=new Point3D(x_side,y_side+z_side,0);
+		upperBase[2]=new Point3D(x_side,y_side+z_side+y_side,0);
+		upperBase[3]=new Point3D(0,y_side+z_side+y_side,0);
+
+		lowerBase=new Point3D[N_FACES];
+
+		lowerBase[0]=new Point3D(0,0,0);
+		lowerBase[1]=new Point3D(0,y_side,0);
+		lowerBase[2]=new Point3D(x_side,y_side,0);
+		lowerBase[3]=new Point3D(x_side,0,0);
+
+
+
 		lateralFaces=new Point3D[N_FACES+1][N_PARALLELS];
 
 		for(int j=0;j<N_PARALLELS;j++){
 
 			//texture is open and periodical:
-			
-			double x=0; 
+
+			double x=0;
 
 			for (int i = 0; i <=N_FACES; i++) {
 
-			
+
 				double y=y_side+z_side*j;
 
 				lateralFaces[i][j]=new Point3D(x,y,0);
-				
+
 				double dx=x_side;
-				
+
 				if(i%2==1)
 					dx=y_side;
-				
+
 				x+=dx;
 
 			}
-			
-		}	
-		
+
+		}
+
 		initMesh();
 	}
-	
 
-	
+
+
 	@Override
 public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
-		
+
 		isTextureDrawing=true;
 
 
-		
+
 		texture_side_dy=(int) (z_side/(N_PARALLELS-1));
 		texture_side_dx=(int) (len/(N_FACES));
-		
+
 
 		Color backgroundColor=Color.green;
 
-		
+
 		IMG_WIDTH=(int) len+2*texture_x0;
 		IMG_HEIGHT=(int) (z_side+y_side*2)+2*texture_y0;
 
-		
+
 		BufferedImage buf=new BufferedImage(IMG_WIDTH,IMG_HEIGHT,BufferedImage.TYPE_BYTE_INDEXED);
-	
+
 		try {
 
 
 				Graphics2D bufGraphics=(Graphics2D)buf.getGraphics();
- 
+
 				bufGraphics.setColor(backgroundColor);
 				bufGraphics.fillRect(0,0,IMG_WIDTH,IMG_HEIGHT);
 
@@ -112,102 +112,100 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 				//draw lines for reference
 
 				bufGraphics.setColor(Color.RED);
+
 				bufGraphics.setStroke(new BasicStroke(0.1f));
-				
+
 				for (int j = 0; j <upperBase.length; j++) {
 
 					double x0= calX(upperBase[j].x);
 					double y0= calY(upperBase[j].y);
-					
+
 					double x1= calX(upperBase[(j+1)%upperBase.length].x);
 					double y1= calY(upperBase[(j+1)%upperBase.length].y);
 
-					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y1);	 
+					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y1);
 
 				}
 
-				
 				//lowerbase
-		
-				bufGraphics.setColor(Color.BLUE);
-				
+
 				for (int j = 0; j <lowerBase.length; j++) {
 
 					double x0= calX(lowerBase[j].x);
 					double y0= calY(lowerBase[j].y);
-					
+
 					double x1= calX(lowerBase[(j+1)%lowerBase.length].x);
 					double y1= calY(lowerBase[(j+1)%lowerBase.length].y);
 
-					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y1);			
+					bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y1);
 
 				}
 
 
 				//lateral surface
-				bufGraphics.setColor(new Color(0,0,0));
 
+				bufGraphics.setColor(new Color(0,0,0));
 
 				for(int j=0;j<N_PARALLELS-1;j++){
 
 					//texture is open and periodical:
 
-					for (int i = 0; i <N_FACES; i++) { 
+					for (int i = 0; i <N_FACES; i++) {
 
 						double x0=calX(lateralFaces[i][j].x);
 						double x1=calX(lateralFaces[i+1][j].x);
 						double y0=calY(lateralFaces[i][j].y);
 						double y1=calY(lateralFaces[i][j+1].y);
-						
+
 						bufGraphics.drawLine((int)x0,(int)y0,(int)x1,(int)y0);
 						bufGraphics.drawLine((int)x1,(int)y0,(int)x1,(int)y1);
 						bufGraphics.drawLine((int)x1,(int)y1,(int)x0,(int)y1);
 						bufGraphics.drawLine((int)x0,(int)y1,(int)x0,(int)y0);
 					}
 
-				}	
-				
-			
+				}
+
+
 				ImageIO.write(buf,"gif",file);
-			
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	private ArrayList<Point3D> buildTexturePoints() {
-		
+
 		isTextureDrawing=false;
-		
+
 		int size=2*N_FACES+	(N_FACES+1)*(N_PARALLELS);
-		
+
 		ArrayList<Point3D> texture_points=new ArrayList<Point3D>(size);
-		
+
 		int count=0;
 		//upperbase
 
 		for (int j = 0; j <upperBase.length; j++) {
 
-			
-			
+
+
 			double x= calX(upperBase[j].x);
 			double y= calY(upperBase[j].y);
 
-			Point3D p=new Point3D(x,y,0);			
+			Point3D p=new Point3D(x,y,0);
 			set(texture_points,count++,p);
 
 
 		}
-	
-		
+
+
 		for (int j = 0; j <lowerBase.length; j++) {
 
 			double x= calX(lowerBase[j].x);
 			double y= calY(lowerBase[j].y);
 
-			Point3D p=new Point3D(x,y,0);			
+			Point3D p=new Point3D(x,y,0);
 			set(texture_points,count++,p);
 
 		}
@@ -231,14 +229,14 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 				//System.out.print(texIndex+"\t");
 				set(texture_points,texIndex,p);
 			}
-			
-		}	
-		
+
+		}
+
 		return texture_points;
 	}
 	@Override
 	public double calX(double x){
-		
+
 		return texture_x0+x;
 	}
 	@Override
@@ -248,30 +246,30 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 		else
 			return texture_y0+y;
 	}
-	
+
 	private void initMesh( ) {
-		
+
 		points=new ArrayList<Point3D>();
 		polyData=new ArrayList<LineData>();
-		
+
 		n=0;
 
 		BPoint[][] aPoints=new BPoint[N_PARALLELS][N_FACES];
-		
-		aPoints[0][0]=addBPoint(0,0,0);			
-		aPoints[0][1]=addBPoint(x_side,0,0);	
-		aPoints[0][2]=addBPoint(x_side,y_side,0);	
+
+		aPoints[0][0]=addBPoint(0,0,0);
+		aPoints[0][1]=addBPoint(x_side,0,0);
+		aPoints[0][2]=addBPoint(x_side,y_side,0);
 		aPoints[0][3]=addBPoint(0,y_side,0);
-		
-		aPoints[1][0]=addBPoint(0,0,z_side);			
-		aPoints[1][1]=addBPoint(x_side,0,z_side);	
-		aPoints[1][2]=addBPoint(x_side,y_side,z_side);	
-		aPoints[1][3]=addBPoint(0,y_side,z_side);	
 
-		for(int j=0;j<N_PARALLELS;j++){  
+		aPoints[1][0]=addBPoint(0,0,z_side);
+		aPoints[1][1]=addBPoint(x_side,0,z_side);
+		aPoints[1][2]=addBPoint(x_side,y_side,z_side);
+		aPoints[1][3]=addBPoint(0,y_side,z_side);
 
-				for (int i = 0; i <N_FACES; i++) {  
-					
+		for(int j=0;j<N_PARALLELS;j++){
+
+				for (int i = 0; i <N_FACES; i++) {
+
 					points.set(aPoints[j][i].getIndex(),aPoints[j][i]);
 
 				}
@@ -281,14 +279,14 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 
 
 		//////// texture points
-			
-	
-			
+
+
+
 		texture_points=buildTexturePoints();
-		
+
 		LineData lowerBase=new LineData();
 		LineData upperBase=new LineData();
-		
+
 		int count=0;
 
 		for (int j = 0; j <N_FACES; j++) {
@@ -297,7 +295,7 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 
 			upperBase.setData(""+Renderer3D.getFace(upperBase,points));
 
-		}	
+		}
 
 		for (int j = N_FACES-1; j >=0; j--) {
 
@@ -309,12 +307,12 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 		polyData.add(upperBase);
 		polyData.add(lowerBase);
 
-		for(int j=0;j<N_PARALLELS-1;j++){ 
+		for(int j=0;j<N_PARALLELS-1;j++){
 
 
 
 
-			for (int i = 0; i <N_FACES; i++) { 
+			for (int i = 0; i <N_FACES; i++) {
 
 
 				LineData ld=new LineData();
@@ -322,31 +320,31 @@ public void saveBaseCubicTexture(PolygonMesh mesh, File file) {
 				int texIndex=count+f(i,j,N_FACES+1,N_PARALLELS);
 				//System.out.print(texIndex+"\t");
 				ld.addIndex(aPoints[j][i].getIndex(),texIndex,0,0);
-				
+
 				texIndex=count+f(i+1,j,N_FACES+1,N_PARALLELS);
 				//System.out.print(texIndex+"\t");
 				ld.addIndex(aPoints[j][(i+1)%N_FACES].getIndex(),texIndex,0,0);
-				
+
 				texIndex=count+f(i+1,j+1,N_FACES+1,N_PARALLELS);
 				//System.out.print(texIndex+"\t");
 				ld.addIndex(aPoints[j+1][(i+1)%N_FACES].getIndex(),texIndex,0,0);
-				
+
 				texIndex=count+f(i,j+1,N_FACES+1,N_PARALLELS);
 				//System.out.print(texIndex+"\t");
 				ld.addIndex(aPoints[j+1][i].getIndex(),texIndex,0,0);
 
 				ld.setData(""+Renderer3D.getFace(ld,points));
-				
-				
+
+
 
 				polyData.add(ld);
 
 			}
-		
+
 
 		}
 
 
 	}
-	
+
 }
--- Driving/src/com/editors/models/CubeModel.java
+++ Driving/src/com/editors/models/CubeModel.java
@@ -9,12 +9,12 @@ import java.util.Vector;
 import com.Point3D;
 
 public class CubeModel extends MeshModel{
-	
+
 	private double dx=100;
 	private double dy=200;
 	private double dz=300;
 
-	
+
 	private int bx=10;
 	private int by=10;
 
@@ -24,19 +24,20 @@ public class CubeModel extends MeshModel{
 		this.dy = dy;
 		this.dz = dz;
 	}
-	
+
+	@Override
 	public void printMeshData(PrintWriter pw) {
-		
-		super.printMeshData(pw);		
-		
+
+		super.printMeshData(pw);
+
 		print(pw,"f=[1]4/14 5/15 6/16 7/17");//TOP
-		
+
 		print(pw,"f=[0]0/4 1/5 5/10 4/9");//BACK
 		print(pw,"f=[3]1/5 2/6 6/11 5/10");//RIGHT
 		print(pw,"f=[4]2/6 3/7 7/12 6/11");//FRONT
-		print(pw,"f=[2]3/7 0/8 4/13 7/12");///LEFT		
+		print(pw,"f=[2]3/7 0/8 4/13 7/12");///LEFT
 		print(pw,"f=[5]0/0 3/1 2/2 1/3");//BOTTOM
-		
+
 	}
 
 
@@ -45,42 +46,42 @@ public class CubeModel extends MeshModel{
 
     	buildBody();
 
-    	buildTexture();    
-    	
+    	buildTexture();
+
     }
 
 
 	private void buildTexture() {
-		
+
 		texturePoints=new Vector<Point3D>();
 
     	//lower base
     	double y=by;
     	double x=bx;
-    	
+
      	addTPoint(x,y,0);
     	seqTPoint(dx,0);
     	seqTPoint(0,dy);
     	seqTPoint(-dx,0);
-    	
+
     	//faces
     	y=by+dy;
-        
+
     	addTPoint(x,y,0);
     	seqTPoint(dx,0);
     	seqTPoint(dy,0);
     	seqTPoint(dx,0);
-    	seqTPoint(dy,0);  	
-    	
-    	
+    	seqTPoint(dy,0);
+
+
     	addTPoint(x,y+dz,0);
     	seqTPoint(dx,0);
     	seqTPoint(dy,0);
     	seqTPoint(dx,0);
     	seqTPoint(dy,0);
-    	
+
     	y=by+dy+dz;
-    	
+
     	//upper base
       	addTPoint(x,y,0);
     	seqTPoint(dx,0);
@@ -90,56 +91,57 @@ public class CubeModel extends MeshModel{
 
 		IMG_WIDTH=(int) (2*dy+2*dx+2*bx);
 		IMG_HEIGHT=(int) (dy*2+dz+2*by);
-		
+
 	}
 
 	private void buildBody() {
-		
+
     	points=new Vector<Point3D>();
-    	
+
     	//lower and upper base
     	for(int k=0;k<2;k++){
-    		
+
     		double z=dz*k;
-    		
+
     		addPoint(0.0,0.0,z);
     		addPoint(dx,0.0,z);
     		addPoint(dx,dy,z);
     		addPoint(0.0,dy,z);
-    		
+
     	}
-		
+
 	}
 
+	@Override
 	public void printTexture(Graphics2D bg) {
-    
+
 
 		//draw lines for reference
 
-		bg.setColor(Color.RED);
+		bg.setColor(Color.BLUE);
 		bg.setStroke(new BasicStroke(0.1f));
-		
+
 		//lower base
 		printTexturePolygon(bg,0,1,2,3);
-		
+
 		//lateral faces
 		bg.setColor(Color.BLACK);
 		printTexturePolygon(bg,4,5,10,9);
-		
+
 		printTextureLine(bg,5,6,11,10);
-		
+
 		printTextureLine(bg,6,7,12,11);
-		
+
 		printTextureLine(bg,7,8,13,12);
 
-		
+
 		//upper base
 		bg.setColor(Color.BLUE);
 		printTexturePolygon(bg,14,15,16,17);
-	
-	
+
+
     }
-    
+
     String ub0="17-16";
     String ub1="14-15";
     String lf0="09-10-11-12-13";
